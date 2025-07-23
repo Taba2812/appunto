cd196e89992af639118ac9f2228bc79d62d21c33
Gabriele Tabarelli de Fatis <earlgibe@DESKTOP-6J7SSQH.>
2025-07-23 17:13:59 +0200

    Add DELETE User

--- appunto/src/app/api/users/[id]/route.ts
+++ appunto/src/app/api/users/[id]/route.ts
@@ -4,36 +4,74 @@ import { NextResponse } from 'next/server';
 import { ObjectId } from 'mongodb';
 
 export async function GET(request: NextRequest, { params }: { params: Promise<{ id: string }> },) {
-  try {
-    const id = (await params).id;
+    try {
+        const id = (await params).id;
 
-    // Validate ObjectId format
-    if (!ObjectId.isValid(id)) {
-      return NextResponse.json(
-        { message: 'Invalid user ID format' },
-        { status: 400 }
-      );
+        // Validate ObjectId format
+        if (!ObjectId.isValid(id)) {
+            return NextResponse.json(
+                { message: 'Invalid user ID format' },
+                { status: 400 }
+            );
+        }
+
+        const client = await clientPromise;
+        const db = client.db('appunto');
+        const user = await db.collection('users').findOne({ _id: new ObjectId(id) })
+
+        if (!user) {
+            return NextResponse.json(
+                { message: 'User not found' },
+                { status: 404 }
+            );
+        }
+
+        return new Response(JSON.stringify(user), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+    } catch (error) {
+        return NextResponse.json(
+            { message: 'Database error', error },
+            { status: 500 }
+        );
     }
-    
-    const client = await clientPromise;
-    const db = client.db('appunto');
-    const user = await db.collection('users').findOne({ _id: new ObjectId(id) })
+}
 
-    if (!user) {
-      return NextResponse.json(
-        { message: 'User not found' },
-        { status: 404 }
-      );
+export async function DELETE(request: NextRequest, { params }: { params: Promise<{ id: string }> },) {
+    try {
+        const id = (await params).id;
+
+        // Validate ObjectId format
+        if (!ObjectId.isValid(id)) {
+            return NextResponse.json(
+                { message: 'Invalid user ID format' },
+                { status: 400 }
+            );
+        }
+
+        const client = await clientPromise;
+        const db = client.db('appunto');
+        const user = await db.collection('users').deleteOne({ _id: new ObjectId(id) })
+
+        if (user.deletedCount == 0) {
+            return NextResponse.json(
+                { message: 'User not found' },
+                { status: 204 }
+            );
+        }
+
+        return NextResponse.json(
+            { message: 'User deleted' },
+            { status: 200 }
+        );
+
+
+    } catch(error) {
+        return NextResponse.json(
+            { message: 'Database error', error },
+            { status: 500 }
+        );
     }
-
-    return new Response(JSON.stringify(user), {
-        status: 200,
-        headers: { 'Content-Type': 'application/json' },
-    });
-  } catch (error) {
-    return NextResponse.json(
-      { message: 'Database error', error },
-      { status: 500 }
-    );
-  }
 }
