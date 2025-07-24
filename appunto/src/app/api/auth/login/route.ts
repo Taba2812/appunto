d43e2f24252df9b175c5c1ca429044d9a0637514
Gabriele Tabarelli de Fatis <earlgibe@DESKTOP-6J7SSQH.>
2025-07-24 17:12:40 +0200

    Change auth URI

---
+++ appunto/src/app/api/auth/login/route.ts
@@ -0,0 +1,49 @@
+import clientPromise from '@/lib/mongodb';
+import { NextResponse } from 'next/server';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+export async function POST(request: Request) {
+    try {
+        const client = await clientPromise;
+        const db = client.db('appunto');
+        const users = db.collection('users');
+
+        const { username, password } = await request.json();
+        const user = await users.findOne({
+            username: username.toUpperCase()
+        });
+
+        if(!user){
+            return new Response(
+                JSON.stringify({ message: 'Username does not exist' }),
+                { status: 404, headers: {'Content-Type': 'application/json'} }
+            )
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if(!isPasswordCorrect){
+            return new Response(
+                JSON.stringify({ message: 'Password is incorrect' }),
+                { status: 401, headers: {'Content-Type': 'application/json'} }
+            )
+        }
+
+        const token = jwt.sign(
+            { userId: user._id, username: user.username, role: user.role },
+            process.env.JWT_SECRET!,
+            { expiresIn: '7d' }
+        );
+
+        return new Response(
+            JSON.stringify( { message: "Login successful", token } ),
+            { status: 200, headers: { 'Content-Type': 'application/json' } }
+        );
+
+    } catch(error) {
+        return NextResponse.json(
+            { message: 'Database error', error },
+            { status: 500 }
+        )
+    }
+}
---
+++ appunto/src/app/api/auth/signup/route.ts
@@ -0,0 +1,40 @@
+import clientPromise from '@/lib/mongodb';
+import { NextResponse } from 'next/server';
+import { User } from '@/lib/User';
+
+export async function POST(request: Request) {
+    try{
+        const client = await clientPromise;
+        const db = client.db('appunto');
+        const users = db.collection('users');
+
+        const { username, password, email } = await request.json();
+        const newUser = new User(username, password, email);
+
+        const existingUser = await users.findOne({
+            $or: [ { email: newUser.email }, { username: newUser.username } ]
+        });
+
+        if(existingUser){
+            return new Response(
+                JSON.stringify({ message: 'User with that email or username already exists.' }),
+                { status: 409, headers: {'Content-Type': 'application/json'} }
+            );
+        }
+
+        const result = await users.insertOne(({
+            ...newUser,
+            timestamp: new Date(),
+        }))
+
+        return new Response(JSON.stringify({ _id: result.insertedId, ...newUser }), {
+            status: 201,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    } catch (error) {
+        return NextResponse.json(
+            { message: 'Database error', error },
+            { status: 500 }
+        );
+    }
+}
